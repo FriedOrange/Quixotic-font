ab2788a2666854642ca10deb08b476a0b836f128
Brad Neil <brad@friedorange.xyz>
2023-01-09 14:42:02 +1100

    Add segments d and e

--- source/generate.py
+++ source/generate.py
@@ -9,8 +9,11 @@ ASPECT_RATIO = -(1 - math.sqrt(5)) / 2
 # SEGMENT_THICKNESS = sys.argv[2]
 SEGMENT_THICKNESS = 80
 SEGMENT_GAP = 10
+CHARACTER_GAP = 200
+VERTICAL_BALANCE_FACTOR = 0.5
 
 SEGMENT_DEFINITIONS = {
+	"space": "",
 	"zero": "abcdef",
 	"one": "bc",
 	"two": "abgned",
@@ -22,33 +25,31 @@ SEGMENT_DEFINITIONS = {
 	"eight": "abcdefgh",
 	"nine": "gnfabcd",
 	"A": "abcefgn",
-	"B": "",
-	"C": "",
-	"D": "",
-	"E": "",
-	"F": "",
-	"G": "",
-	"H": "",
-	"I": "",
-	"J": "",
-	"K": "",
-	"L": "",
-	"M": "",
-	"N": "",
-	"O": "",
-	"P": "",
-	"Q": "",
-	"R": "",
-	"S": "",
-	"T": "",
-	"U": "",
-	"V": "",
-	"W": "",
-	"X": "",
-	"Y": "",
-	"Z": "",
-	"": "",
-	"": "",
+	"B": "abcgnil",
+	"C": "afed",
+	"D": "abcdil",
+	"E": "afegnd",
+	"F": "afegn",
+	"G": "afedcgn",
+	"H": "fegnbc",
+	"I": "aeld",
+	"J": "bcde",
+	"K": "fegjn",
+	"L": "fed",
+	"M": "efhjbc",
+	"N": "efimcb",
+	"O": "abcdef",
+	"P": "efabgn",
+	"Q": "ebcdefm",
+	"R": "efabgnm",
+	"S": "afgncd",
+	"T": "ail",
+	"U": "fedcb",
+	"V": "fekj",
+	"W": "fekmcd",
+	"X": "hjkm",
+	"Y": "hjl",
+	"Z": "ajkd",
 }
 
 def main():
@@ -57,6 +58,40 @@ def main():
 	font = fontforge.font()
 	font.encoding = "UnicodeFull"
 	font.em = EM_SIZE
+
+	# calculate parameters
+	width = int(CAP_HEIGHT * ASPECT_RATIO + CHARACTER_GAP)
+	side_bearing = int(CHARACTER_GAP / 2)
+	corner_gap = int(math.sqrt(SEGMENT_GAP**2 / 2))
+	vertical_midpoint = CAP_HEIGHT * VERTICAL_BALANCE_FACTOR
+
+	# draw individual segments
+	font.createChar(-1, "segment-d")
+	pen = font["segment-d"].glyphPen()
+	pen.moveTo(side_bearing + SEGMENT_THICKNESS // 2 + corner_gap, SEGMENT_THICKNESS // 2)
+	pen.lineTo(side_bearing + SEGMENT_THICKNESS + corner_gap, SEGMENT_THICKNESS)
+	pen.lineTo(width - (side_bearing + SEGMENT_THICKNESS + corner_gap), SEGMENT_THICKNESS)
+	pen.lineTo(width - (side_bearing + SEGMENT_THICKNESS // 2 + corner_gap), SEGMENT_THICKNESS // 2)
+	pen.lineTo(width - (side_bearing + SEGMENT_THICKNESS + corner_gap), 0)
+	pen.lineTo(side_bearing + SEGMENT_THICKNESS + corner_gap, 0)
+	pen.closePath()
+	pen = None
+	font["segment-d"].width = width
+
+	font.createChar(-1, "segment-e")
+	pen = font["segment-e"].glyphPen()
+	pen.moveTo(side_bearing + SEGMENT_THICKNESS // 2, SEGMENT_THICKNESS // 2 + corner_gap)
+	pen.lineTo(side_bearing, SEGMENT_THICKNESS + corner_gap)
+	pen.lineTo(side_bearing, vertical_midpoint - SEGMENT_THICKNESS // 2 - corner_gap)
+	pen.lineTo(side_bearing + SEGMENT_THICKNESS // 2, vertical_midpoint - corner_gap)
+	pen.lineTo(side_bearing + SEGMENT_THICKNESS, vertical_midpoint - SEGMENT_THICKNESS // 2 - corner_gap)
+	pen.lineTo(side_bearing + SEGMENT_THICKNESS, SEGMENT_THICKNESS + corner_gap)
+	pen.closePath()
+	pen = None
+	font["segment-e"].width = width
+
+	# finished
+	font.save("source\\temp\\temp.sfd")
 
 if __name__ == "__main__":
 	main()
