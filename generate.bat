21feb00b8d1dd8b3f986efe9cc6fa8c62b894106
Brad Neil <brad@friedorange.xyz>
2023-01-16 18:19:13 +1100

    Generate intermediate UFO sources

--- generate.bat
+++ generate.bat
@@ -1,1 +1,10 @@
 fontforge -script source\generate.py 56 7 source\7.csv
+sfd2ufo source\temp.sfd source\QuixoticSeven-Light.ufo
+fontforge -script source\generate.py 94 7 source\7.csv
+sfd2ufo source\temp.sfd source\QuixoticSeven-Medium.ufo
+fontforge -script source\generate.py 130 7 source\7.csv
+sfd2ufo source\temp.sfd source\QuixoticSeven-Bold.ufo
+fontforge -script source\generate.py 56 14 source\14.csv
+sfd2ufo source\temp.sfd source\QuixoticFourteen.ufo
+fontforge -script source\generate.py 56 16 source\16.csv
+sfd2ufo source\temp.sfd source\QuixoticSixteen.ufo
