c9545990e1a879cf468ac45a59d19c7a930d5e8e
Brad Neil <brad@friedorange.xyz>
2023-01-16 18:24:39 +1100

    Generate Seven Regular

--- generate.bat
+++ generate.bat
@@ -1,7 +1,7 @@
 fontforge -script source\generate.py 56 7 source\7.csv
 sfd2ufo source\temp.sfd source\QuixoticSeven-Light.ufo
-fontforge -script source\generate.py 94 7 source\7.csv
-sfd2ufo source\temp.sfd source\QuixoticSeven-Medium.ufo
+fontforge -script source\generate.py 76 7 source\7.csv
+sfd2ufo source\temp.sfd source\QuixoticSeven-Regular.ufo
 fontforge -script source\generate.py 130 7 source\7.csv
 sfd2ufo source\temp.sfd source\QuixoticSeven-Bold.ufo
 fontforge -script source\generate.py 56 14 source\14.csv
