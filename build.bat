ca0304a0773d25eaa4fc9c7cbe2eff8edb44c1d7
Brad Neil <brad@friedorange.xyz>
2023-01-18 00:05:42 +1100

    Generate all weights

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
-mkdir fonts
-cd fonts
-fontmake ..\source\QuixoticSeven.designspace -o variable
-fontmake ..\source\QuixoticSeven.designspace -i
-cd ..
+@REM mkdir fonts
+@REM cd fonts
+@REM fontmake ..\source\QuixoticSeven.designspace -o variable
+@REM fontmake ..\source\QuixoticSeven.designspace -i
+@REM cd ..
+
+@cd source
+python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-builder.py config.yaml
+@cd ..
--- generate.bat
+++ generate.bat
@@ -2,6 +2,10 @@ fontforge -script source\generate.py 56 7 source\7.csv 0.535
 sfd2ufo source\temp\temp.sfd source\QuixoticSeven-Light.ufo
 fontforge -script source\generate.py 76 7 source\7.csv 0.5475
 sfd2ufo source\temp\temp.sfd source\QuixoticSeven-Regular.ufo
+fontforge -script source\generate.py 94 7 source\7.csv 0.5475
+sfd2ufo source\temp\temp.sfd source\QuixoticSeven-Medium.ufo
+fontforge -script source\generate.py 112 7 source\7.csv 0.5475
+sfd2ufo source\temp\temp.sfd source\QuixoticSeven-Semibold.ufo
 fontforge -script source\generate.py 130 7 source\7.csv 0.5475
 sfd2ufo source\temp\temp.sfd source\QuixoticSeven-Bold.ufo
 fontforge -script source\generate.py 56 14 source\14.csv 0.535
