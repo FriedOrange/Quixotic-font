2aac5eed33688f9df9667d54ef33d2f1ec502c2d
Brad Neil <brad@friedorange.xyz>
2023-01-19 01:33:25 +1100

    Change check profile

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 set PYTHONUTF8=1
 mkdir fonts\fontbakery
-fontbakery check-googlefonts -C -l PASS --html fonts\fontbakery\fontbakery-report.html fonts\ttf\*.ttf > fonts\fontbakery\test.log
+fontbakery check-universal -C -l PASS --html fonts\fontbakery\fontbakery-report.html fonts\ttf\*.ttf > fonts\fontbakery\test.log
