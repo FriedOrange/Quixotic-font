176407f7311dba5bb3df5cb850711453891799b1
Brad Neil <brad@friedorange.xyz>
2023-01-18 20:15:36 +1100

    Generate proofs

--- build.bat
+++ build.bat
@@ -7,3 +7,8 @@
 @cd source
 python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-builder.py config.yaml
 @cd ..
+
+@cd fonts\ttf
+set PYTHONUTF8=1
+python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-gen-html.py proof -o ..\proof QuixoticFourteen.ttf QuixoticSeven-Bold.ttf QuixoticSeven-Light.ttf QuixoticSeven-Medium.ttf QuixoticSeven-Regular.ttf QuixoticSeven-Semibold.ttf QuixoticSixteen.ttf
+@cd ..\..
